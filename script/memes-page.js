fc935aead9754320bd4eb552a737d1e24871fbd2
Oardieldei <alik.want.you@gmail.com>
2026-09-21 19:06:22 +0300

    feat: add burger menu

--- script/memes-page.js
+++ script/memes-page.js
@@ -1,9 +1,11 @@
 import { makeThemesGreatAgain } from "./themes.js"
+import { initBurger } from "./burger.js"
 import { categoriesOnFire } from "./memesPage/categories.js"
 import { fillContaner } from "./memesPage/fillContainer.js"
 import { checkWindowSize } from "./memesPage/resizing.js"
 
 makeThemesGreatAgain()
+initBurger()
 categoriesOnFire()
 fillContaner('all')
 checkWindowSize()
